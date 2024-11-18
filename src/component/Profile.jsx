a7527302650c86fb8e10b21bf9092e0c5c040f14
aravind <aravind284479@gmail.com>
2024-11-18 13:51:06 +0530

    Update : Changed the Profile Pic

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import profilePic from './assests/profile.jpg'
-// import DP from './assests/DP.jpg'
-// import DP from './assests/DP1.jpg'
-import DP from './assests/DP2.jpg'
+import React from "react";
+import profilePic from "./assests/profile.jpg";
+// import DP from "./assests/DP2.jpg";
+import DP2 from "./assests/image.png";
 
 const Profile = () => {
   return (
-    <div className='col-md-6 profile-class'>
+    <div className="col-md-6 profile-class">
       <div id="profile-pic">
-        <img src={DP || profilePic} alt="" />
+        <img src={DP2 || profilePic} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
