3d722412d5f082f6a2139d9a1efce3a47b2476de
aravind <aravind284479@gmail.com>
2025-01-03 14:12:06 +0530

    feat : Optimized profile picture

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -11,25 +11,45 @@ const Contact = () => {
       <h4>
         <strong>CONTACT</strong>
       </h4>
-      <br/>
+      <br />
       <div className="contact-div">
-        <a target="_blank" href="https://api.whatsapp.com/send/?phone=%2B917559027982&text&type=phone_number&app_absent=0" className="contact-child-div">
+        <a
+          target="_blank"
+          href="https://api.whatsapp.com/send/?phone=%2B917559027982&text&type=phone_number&app_absent=0"
+          className="contact-child-div"
+        >
           <img src={whatsapp} alt="whatsapp" />
           <p>WhatsApp</p>
         </a>
-        <a target="_blank" href="https://github.com/aravindvjn" className="contact-child-div">
+        <a
+          target="_blank"
+          href="https://github.com/aravindvjn"
+          className="contact-child-div"
+        >
           <img src={github} alt="github" />
           <p>Github</p>
         </a>
-        <a target="_blank" href="mailto:aravind284479@gmail.com" className="contact-child-div">
+        <a
+          target="_blank"
+          href="mailto:aravind284479@gmail.com"
+          className="contact-child-div"
+        >
           <img src={gmailClr} alt="gmail" />
           <p>e-Mail</p>
         </a>
-        <a target="_blank" href="https://www.instagram.com/arawindh?igsh=MXY1eGY5em1iNmcxYw==" className="contact-child-div">
+        <a
+          target="_blank"
+          href="https://www.instagram.com/6windh"
+          className="contact-child-div"
+        >
           <img src={instagram} alt="instagram" />
           <p>Instagram</p>
         </a>
-        <a target="_blank" href="https://www.linkedin.com/in/aravind-vijayan-aa0a27214?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="contact-child-div">
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/aravind-vijayan-aa0a27214?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          className="contact-child-div"
+        >
           <img src={linkedinClr} alt="linked in" />
           <p>LinkedIn</p>
         </a>
--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import image1280 from "./assests/imagex1280.png";
+import image1024 from "./assests/imagex1024.png";
+import image640 from "./assests/imagex640.png";
 import profilePic from "./assests/profile.jpg";
-// import DP from "./assests/DP2.jpg";
-import DP2 from "./assests/image.png";
 
 const Profile = () => {
   return (
     <div className="col-md-6 profile-class">
       <div id="profile-pic">
-        <img src={DP2 || profilePic} alt="" />
+        <img
+          src={image640 || profilePic}
+          srcSet={`
+    ${image640} 640w,
+    ${image1024} 1024w,
+    ${image1280} 1280w
+  `}
+          sizes="(max-width: 640px) 100vw,
+         (max-width: 1024px) 50vw,
+         33vw"
+          alt="Profile Picture"
+          loading="lazy"
+        />
       </div>
     </div>
   );
